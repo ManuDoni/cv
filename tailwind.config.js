967b97546aed7c067bb90d09759f6c1e57f174ef
Manuel Donini <manueldonini94@gmail.com>
2020-03-29 18:49:47 +0200

    Sistema il testo del curriculum.
    Rende la pagina responsive.
    
    Crea il breakpoint 'print' apposito per la stampa.
    
    Aggiunge il codice QR del link di questa repo.\nRinomina i file css

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  theme: {
+    extend: {
+      screens: {
+        'print': {'raw': 'print'},
+        // => @media  print { ... }
+      }
+    },
+  },
+  variants: {},
+  plugins: [],
+}
