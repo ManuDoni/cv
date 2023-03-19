1b134d989cd22e83b3fc6204ac29cb588341737e
ManuDoni <manueldonini94@gmail.com>
2023-03-19 23:34:01 +0100

    Rimuove definizione del custom screen "print", perché è stato implementato nella versione v3 di tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,10 +10,6 @@ module.exports = {
         primary: colors.sky,
         grey: colors.slate,
       },
-      screens: {
-        'print': {'raw': 'print'},
-        // => @media  print { ... }
-      },
       spacing: {
         '28': '7rem', // w-28 = 7rem
       }
